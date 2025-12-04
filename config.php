04a642efd6bd30992efda129acf9c9c3e18838cd
ezzahra12 <blnfatima64@gmail.com>
2025-12-04 09:39:32 +0100

    connexion avec la base de donnee

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$host= "localhost";
+$dbname="wallet";
+$username="root";
+$password="root";
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname",$username,$password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    
+}catch(PDOException $e){
+    die("erreur de connexion:" . $e->getMessage());
+}
