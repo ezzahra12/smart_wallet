cd2e8c89785e9813f2a483d3c0262bf9423db7b4
ezzahra12 <blnfatima64@gmail.com>
2025-12-05 19:58:20 +0100

    ajouter les requetes sql

---
+++ data.sql
@@ -0,0 +1,49 @@
+create database wallet;
+use	wallet;
+create table incomes(
+ id int primary key auto_increment,
+ amount DECIMAL(10,2) ,
+ desp VARCHAR(255),
+ date_income DATE,
+ created_at date DEFAULT (CURRENT_DATE))
+ 
+ 
+ show tables;
+ describe incomes;
+ 
+ create table expenses(
+ id int primary key auto_increment,
+ amount decimal(10,2),
+ desp varchar(215),
+ date_expense Date,
+ created_at date default (current_date))
+ show tables;
+ describe incomes;
+ 
+ create table categories(
+ id int primary key auto_increment,
+ name varchar(215),
+ type enum('income','expense'),
+ created_at date default (current_date));
+ 
+ 
+
+ describe incomes;
+ 
+ alter table incomes
+ add column category_id int;
+ 
+  alter table incomes
+ add constraint category_id 
+ foreign key (category_id) references categories(id);
+ 
+ 
+ alter table expenses
+ add column category_id int;
+ 
+  alter table expenses
+ add constraint category_id 
+ foreign key (category_id) references categories(id);
+ 
+ 
+ describe expenses;
